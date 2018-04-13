f3c4d86f5a5ecee76d23b07ed6d1de0cdc1c9817
ShemManyu <smanyu48@gmail.com>
2018-04-13 21:46:47 +0300

    Finished UI. All that is remaining is persistance

--- booking.py
+++ booking.py
@@ -1,3 +1,5 @@
+import os
+
 from datetime import datetime
 from flask import Flask, render_template, url_for, request, redirect, flash
 
@@ -5,10 +7,10 @@ app = Flask(__name__)
 rooms = []
 app.config['SECRET_KEY'] = '-\xeb\xa9\xcc\xad\x82\xc8*\x96;\x89<F\x83\x04|>\xf79\xcf\xee\x12\xf7\xc5'
 
-def add_room(roomnumber):
+def add_room(roomnumber, description):
     rooms.append(dict(
         roomnumber = roomnumber,
-        user = "shem",
+        description = description,
         date = datetime.utcnow()
     ))
 
@@ -16,8 +18,13 @@ def add_room(roomnumber):
 def index():
     if request.method == "POST":
         roomnumber = request.form['roomnumber']
-        add_room(roomnumber)
+        description = request.form['description']
+        add_room(roomnumber, description)
     return render_template('index.html', rooms=rooms)
+
+@app.route('/room', methods=['GET', 'POST'])
+def room():
+    return render_template('room.html')
 
 @app.errorhandler(404)
 def page_not_found(e):
